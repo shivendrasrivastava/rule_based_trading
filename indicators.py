535f146107a2f338d0e26712138c2bf506420d0a
Trang Nguyen <ntrang086@gmail.com>
2018-01-22 09:50:55 -0500

    feat: Implement get_bollinger_bands(), compute_bollinger_value(), and plot_bollinger()

--- indicators.py
+++ indicators.py
@@ -35,3 +35,77 @@ def get_sma_indicator(price, rolling_mean):
     """
     return price / rolling_mean - 1
 
+
+def get_bollinger_bands(rolling_mean, rolling_std, num_std=2):
+    """
+    Calculate upper and lower Bollinger Bands
+
+    Parameters:
+    rolling_mean: Rolling mean of a series
+    rolling_meanstd: Rolling std of a series
+    num_std: Number of standard deviations for the bands
+
+    Returns: Bollinger upper band and lower band
+    """
+    upper_band = rolling_mean + rolling_std * num_std
+    lower_band = rolling_mean - rolling_std * num_std
+    return upper_band, lower_band
+
+
+def compute_bollinger_value(price, rolling_mean, rolling_std):
+    """
+    Output a value indicating how many standard deviations a price is from the mean
+
+    Parameters:
+    price: Price, typically adjusted close price, series of a symbol
+    rolling_mean: Rolling mean of a series
+    rolling_std: Rolling std of a series
+
+    Returns:
+    bollinger_val: the number of standard deviations a price is from the mean
+    """
+
+    bollinger_val = (price - rolling_mean) / rolling_std
+    return bollinger_val
+
+
+def plot_bollinger(symbol, sym_price, upper_band, lower_band, bollinger_val, num_std=1):
+    """
+    Plot Bollinger bands and value for a symbol
+
+    Parameters:
+    symbol: A symbol of interest
+    sym_price: Price, typically adjusted close price, series of symbol
+    upper_band: Bollinger upper band
+    lower_band: Bollinger lower band
+    bollinger_val: The number of standard deviations a price is from the mean
+    num_std: Number of standard deviations for the bands
+
+    Returns:
+    Plot two subplots, one for the Adjusted Close Price and Bollinger bands, the other 
+    for the Bollinger value
+    """
+    
+    # Create 2 subplots
+    # First subplot: symbol"s adjusted close price, rolling mean and Bollinger Bands
+    f, ax = plt.subplots(2, sharex=True)
+    ax[0].fill_between(upper_band.index, upper_band, lower_band, color="gray", alpha=0.4, 
+        linewidth=2.0, label="Region btwn Bollinger Bands")
+    ax[0].plot(sym_price, label=symbol + " Adjusted Close", color="b")
+    ax[0].set_title("{} Adjusted Close with Bollinger Bands (num. of std = {})".format(
+        symbol, num_std))
+    ax[0].set_ylabel("Adjusted Close Price")
+    ax[0].legend(loc="upper center")
+
+    # Second subplot: the bollinger value
+    ax[1].axhspan(-num_std, num_std, color="gray", alpha=0.4, linewidth=2.0,
+        label="Region btwn {} & {} std".format(-num_std, num_std))
+    ax[1].plot(bollinger_val, label=symbol + " Bollinger Value", color="b")
+    ax[1].set_title("{} Bollinger Value)".format(symbol))
+    ax[1].set_xlabel("Date")
+    ax[1].set_ylabel("Bollinger Value")
+    ax[1].set_xlim(bollinger_val.index.min(), bollinger_val.index.max())
+    ax[1].legend(loc="upper center")
+    plt.show()
+
+
